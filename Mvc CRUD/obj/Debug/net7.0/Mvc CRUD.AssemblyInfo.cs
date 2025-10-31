b229f9ceec6fd2a72415196f1a40afa30c8769c1
kagiso <kagiso@ec1.co.za>
2025-10-31 18:30:26 +0200

    Added a SignalR messaging

--- Mvc CRUD/obj/Debug/net7.0/Mvc CRUD.AssemblyInfo.cs
+++ Mvc CRUD/obj/Debug/net7.0/Mvc CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mvc CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d16819835aeeacf6639bfffd55aa88b1200f0160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddee6eeb0dd19d43f5b80f9d49c75e570e4d8502")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mvc CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mvc CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
