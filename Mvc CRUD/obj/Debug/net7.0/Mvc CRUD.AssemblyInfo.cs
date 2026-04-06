f6da3dd58cfc7f2d438fb554675fa58b12bb0885
cagegit56 <kagisogilbertho56@gmail.com>
2026-04-06 17:18:08 +0200

    Reduced all event listeners to one and fixed create post card

--- Mvc CRUD/obj/Debug/net7.0/Mvc CRUD.AssemblyInfo.cs
+++ Mvc CRUD/obj/Debug/net7.0/Mvc CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mvc CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87bd73eb558be0fdb32bc54a7b8b4e701d3217c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42d7e6bf92cbe452a1922d2cbfc7208094aa7f77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mvc CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mvc CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
